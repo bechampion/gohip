06808223da847f6d6e668f6a83b5fcb6f73bfa94
Marc Esquerra <esquerra@gmail.com>
2024-06-11 17:20:12 +0100

    Add nix shell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = (import ./shell.nix) pkgs;
+        }
+      );
+}
