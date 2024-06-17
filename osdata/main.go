6d53bf442623d7caa3401c39d9f5f72b3cd9840e
Jeronimo Garcia <garciaj.uk@gmail.com>
2024-06-17 20:48:26 +0100

    Adding a single interface

--- osdata/main.go
+++ osdata/main.go
@@ -1,13 +1,75 @@
 package osdata
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/bechampion/gohip/types"
 	ctypes "github.com/bechampion/gohip/types"
 	"github.com/shirou/gopsutil/v3/host"
-	"github.com/shirou/gopsutil/v3/net"
-	"os"
-	"strings"
 )
+
+type MainInterface struct {
+    Dst      string   `json:"dst"`
+    Gateway  string   `json:"gateway"`
+    Dev      string   `json:"dev"`
+    PrefSrc  string   `json:"prefsrc"`
+    Flags    []string `json:"flags"`
+    UID      int      `json:"uid"`
+    Cache    []string `json:"cache"`
+}
+type MacAddress struct {
+    Address 	string   `json:"address"`
+}
+func GetMac(dev string) (string, error) {
+    cmd := exec.Command("ip", "-json", "link", "show", dev)
+    output, err := cmd.Output()
+    if err != nil {
+        return "", err
+    }
+
+    var address []MacAddress
+    err = json.Unmarshal(output, &address)
+    if err != nil {
+        return "", err
+    }
+	return address[0].Address,nil
+}
+func GetInterfaces() ([]ctypes.NetworkEntry, error) {
+	iface := ctypes.NetworkEntry{}
+    cmd := exec.Command("ip", "-json", "r", "get", "1.1.1.1")
+    output, err := cmd.Output()
+    if err != nil {
+        return nil, err
+    }
+
+    var routes []MainInterface
+    err = json.Unmarshal(output, &routes)
+    if err != nil {
+        return nil, err
+    }
+
+    if len(routes) == 0 {
+        return nil, fmt.Errorf("no route found for destination %s", "1.1.1.1")
+    }
+
+	var ipaddress ctypes.IPEntry
+	ipaddress.Name = routes[0].Gateway
+	iface.IPAddress.Entries  = append(iface.IPAddress.Entries,ipaddress)
+	iface.Name = routes[0].Dev
+	// Let's get the mac
+	mac,err := GetMac(iface.Name)
+	if err != nil {
+		return nil, err
+		}
+	iface.Description="This is your default dev"
+	iface.MacAddress = mac
+	ifaces := []types.NetworkEntry{}
+	ifaces = append(ifaces,iface)
+    return ifaces, nil
+}
 
 func GetHostname() (string, error) {
 	hostname, err := os.Hostname()
@@ -32,32 +94,3 @@ func GetOSVersion() (string, error) {
 	return fmt.Sprintf("%s %s", info.Platform, info.PlatformVersion), nil
 }
 
-func GetInterfaces() ([]ctypes.NetworkEntry, error) {
-	ifaces := []ctypes.NetworkEntry{}
-	interfaces, err := net.Interfaces()
-	if err != nil {
-		return nil, err
-	}
-
-	var i ctypes.NetworkEntry
-	var ips []ctypes.IPEntry
-	var ip ctypes.IPEntry
-	var ipss ctypes.IPAddresses
-	for _, iface := range interfaces {
-		i.Name = iface.Name
-		for _, addr := range iface.Addrs {
-			if strings.Contains(addr.Addr, ":") {
-				// Skip IPv6 addresses for simplicity
-				continue
-			}
-			ip.Name = addr.Addr
-			ips = append(ips, ip)
-		}
-		ipss.Entries = ips
-		i.IPAddress = ipss
-		i.MacAddress = iface.HardwareAddr
-		ifaces = append(ifaces, i)
-	}
-
-	return ifaces, nil
-}
