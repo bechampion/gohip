86915da66788d9a95551ce77a40b3e4538c26b95
Marc Esquerra <esquerra@gmail.com>
2024-06-28 15:00:00 +0100

    This should update the hash every release

--- flake.nix
+++ flake.nix
@@ -22,10 +22,18 @@
             excludedPackages = ["osdata" "others" "systemd" "types"];
           });
 
+          upgrade-nix-gohip-package = pkgs.writeShellScriptBin "upgrade-nix-gohip" ''
+            echo "UPGRADING"
+            echo "pkgs : pkgs.lib.fakeHash" > $WORKSPACE/gohip.vendor.hash.nix
+            NEW_HASH=`nix build .#default 2>&1 | grep " got: " | awk '{print $2}'`
+            echo "pkgs : \"$NEW_HASH\"" > $WORKSPACE/gohip.vendor.hash.nix
+          '';
+          custom-packages = {upgrade-nix-gohip = upgrade-nix-gohip-package;};
+
         in
         with pkgs;
         {
-          devShells.default = (import ./shell.nix) pkgs;
+          devShells.default = (import ./shell.nix) pkgs custom-packages;
           apps = rec {
             gohip = flake-utils.lib.mkApp {
               drv = pkgs.writeShellScriptBin "gohip" ''
@@ -33,10 +41,14 @@
                 "${gohip-package}"/bin/gohip
               '';
             };
+            upgrade-nix-gohip = flake-utils.lib.mkApp {
+              drv = upgrade-nix-gohip-package;
+            };
             default = gohip;
           };
           packages = rec {
             gohip = gohip-package;
+            upgrade-nix-gohip = upgrade-nix-gohip-package;
             default = gohip;
           };
         }
--- shell.nix
+++ shell.nix
@@ -1,14 +1,7 @@
-pkgs :
+pkgs : custom-packages :
 
 let
-  upgrade-nix-gohip = pkgs.writeShellScriptBin "upgrade-nix-gohip" ''
-    echo "UPGRADING"
-    echo "pkgs : pkgs.lib.fakeHash" > $WORKSPACE/gohip.vendor.hash.nix
-    NEW_HASH=`nix build .#default 2>&1 | grep " got: " | awk '{print $2}'`
-    echo "pkgs : \"$NEW_HASH\"" > $WORKSPACE/gohip.vendor.hash.nix
-
-  '';
 in
   pkgs.mkShell {
-    buildInputs = [ pkgs.go upgrade-nix-gohip ];
+    buildInputs = [ pkgs.go custom-packages.upgrade-nix-gohip ];
   }
