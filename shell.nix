1110bb2baf45546ce8026fea0681c4987fd3b7fa
Marc Esquerra <esquerra@gmail.com>
2024-06-28 14:27:09 +0100

    Finish upgrade script

--- shell.nix
+++ shell.nix
@@ -3,6 +3,10 @@ pkgs :
 let
   upgrade-nix-gohip = pkgs.writeShellScriptBin "upgrade-nix-gohip" ''
     echo "UPGRADING"
+    echo "pkgs : pkgs.lib.fakeHash" > $WORKSPACE/gohip.vendor.hash.nix
+    NEW_HASH=`nix build .#default 2>&1 | grep " got: " | awk '{print $2}'`
+    echo "pkgs : \"$NEW_HASH\"" > $WORKSPACE/gohip.vendor.hash.nix
+
   '';
 in
   pkgs.mkShell {
